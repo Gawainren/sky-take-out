48c69808730cadfa83f43083da6d80cb29c54768
Gawainren <2734284877@qq.com>
2024-06-25 01:31:09 +0800

    添加功能—新增员工（代码开发）

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -20,8 +20,8 @@ public interface EmployeeMapper {
      *
      * @param employee
      */
-    @Insert("insert into employee(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user,status) " +
-            "VALUES " +
-            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
+    @Insert("insert into employee(name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user,status) " +
+            "values " +
+            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
     void insert(Employee employee);
 }
